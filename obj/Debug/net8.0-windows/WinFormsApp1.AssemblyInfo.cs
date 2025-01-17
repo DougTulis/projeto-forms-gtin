423ddce2ec3132f2f62006a9ff7ff80a78774126
DougTulis <dengratulis@gmail.com>
2025-01-17 13:15:36 -0300

    Update

--- obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40226eaa46113c28d27adba3582d808e6d8da2ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
